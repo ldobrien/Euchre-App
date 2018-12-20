99f4cc66af912cbee9c403e7e31432d2726cdbbe
Lisa <lisaobrien2009@hotmail.com>
2018-12-19 21:01:53 -0500

    Add Player error messages

--- uweuchre/src/components/AddPlayer.js
+++ uweuchre/src/components/AddPlayer.js
@@ -8,34 +8,38 @@ import { firestoreConnect } from 'react-redux-firebase';
 class AddPlayer extends Component {
     state = {
         Name:"",
-        Skill:""
-        // Submitted: 0
+        Skill:"",
+        error: null,
+        success: null
     }
-    // submitted = 0;
+
     handleSubmit = (e) => {
         e.preventDefault();
         if(this.props.players[this.state.Name]){
             this.setState({
-                error: "Unable to Add Player: Player is not unique"
+                error: "Unable to Add Player: Player is not unique",
+                success: null
             })
         } else {
             this.props.createPlayer(this.state);
+            this.setState({
+                error: null,
+                success: "Player successfully added"
+            })
         }
     }
+
     handleChange = (e) =>{
         this.setState({
             [e.target.name]: e.target.value
         })
     }
+
     render(){
         const { auth } = this.props;
         if(!auth.uid) {
             return <Redirect to="/signin"/>
         }
-        // const submitted = this.state.Submitted === 1 ? 
-        //     <p>Player Sucessfully Added</p>
-        //      : 
-        //     <p>No Players Added</p>;
         return (
             
             <div className="container">
@@ -44,13 +48,14 @@ class AddPlayer extends Component {
                 <form onSubmit={this.handleSubmit}>
                     <label>Add New Player</label>
                     <input type="text" onChange={this.handleChange} name="Name" placeholder="Name"/>
-                    <input type="text" onChange={this.handleChange} name="Skill" placeholder="Skill"/>
                     <button type="submit"> Add </button>
                 </form>
                 <div className="red-text center">
                     {this.state.error ? <p>{this.state.error}</p> : null}
                 </div> 
-            {/* {submitted} */}
+                <div className="black-text center">
+                    {this.state.success ? <p>{this.state.success}</p> : null}
+                </div> 
             </div>
         )
     }
@@ -62,6 +67,7 @@ const mapStatetoProps = (state) => {
         players: state.firestore.data.players
     }
 }
+
 const mapDispatchtoProps = (dispatch) => {
     return {
         createPlayer: (player) => dispatch(createPlayer(player))
